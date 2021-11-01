ee08bbe0d8fdbe6987525918b0b86974da893e47
KING HANTER <83442984+KING-HANTER@users.noreply.github.com>
2021-11-01 16:38:54 +0330

    Add files via upload

---
+++ Logo.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+echo""
+banner() {
+ echo -e $'\e[1;33m\e[0m\e[1;37m      _  _____ _   _  ____      \e[0m''\e[1;33m\e[0m\e[1;32m  _   _    _    _   _ _____ _____ ____   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     | |/ /_ _| \ | |/ ___|     \e[0m''\e[1;33m\e[0m\e[1;32m | | | |  / \  | \ | |_   _| ____|  _ \  \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     | . / | ||  \| | |  _  ███ \e[0m''\e[1;33m\e[0m\e[1;32m | |_| | / _ \ |  \| | | | |  _| | |_) | \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     | . \ | || |\  | |_| |     \e[0m''\e[1;33m\e[0m\e[1;32m |  _  |/ ___ \| |\  | | | | |___|  _ <  \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     |_|\_\___|_| \_|\____|     \e[0m''\e[1;33m\e[0m\e[1;32m |_| |_/_/   \_\_| \_| |_| |_____|_| \_\ \e[0m'
+  echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 3.2] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/Ignitetch/AdvPhishing \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    |K|I|N|G| |H|A|N|T|E|R|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
+      echo ""                                                
+      }
+      banner
